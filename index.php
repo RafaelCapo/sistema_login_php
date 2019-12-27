44f4ce1a5e0d25b32e4b19599ed46913cf181f39
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2019-12-27 09:32:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="box_login">
+		<div class="capa">
+		</div>
+
+		<h1>Faça seu login</h1>
+
+		<form>
+			<input type="email" name="e-mail" placeholder="NOME COMPLETO">
+			<input type="password" name="password" placeholder="E-MAIL">
+
+			<button class="btn-login">LOGAR</button>
+			<span class="msg_error"><i class="fa fa-exclamation-triangle" style="font-size: 16px; color: #FF6D6D; padding-right: 5px;"></i> <p>Tentativa Inválida</p></span>
+		</form>
+	</div>
+</body>
+</html>
