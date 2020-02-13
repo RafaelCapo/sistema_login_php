37927ca94db0fdb3f2c1b5ca847ced64b98c39c8
RafaelCapo <48961695+RafaelCapo@users.noreply.github.com>
2020-02-13 15:36:38 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" href="assets/css/style.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+	session_start();
 
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-	<div class="box_login">
-		<div class="capa">
-		</div>
+	require_once 'app/core/Core.php';
 
-		<h1>Faça seu login</h1>
+	require_once 'lib/Rafael/Database/Connection.php';
 
-		<form>
-			<input type="email" name="e-mail" placeholder="NOME COMPLETO">
-			<input type="password" name="password" placeholder="E-MAIL">
+	require_once 'app/controller/LoginController.php';
+	require_once 'app/controller/DashboardController.php';
+	require_once 'app/model/User.php';
 
-			<button class="btn-login">LOGAR</button>
-			<span class="msg_error"><i class="fa fa-exclamation-triangle" style="font-size: 16px; color: #FF6D6D; padding-right: 5px;"></i> <p>Tentativa Inválida</p></span>
-		</form>
-	</div>
-</body>
-</html>
+	require_once 'vendor/autoload.php';
+
+	$core = new Core;
+	echo $core->start($_GET);
